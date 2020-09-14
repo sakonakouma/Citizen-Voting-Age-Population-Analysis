081e874ac1ba77e378e381d0fcbad952a5533e0b
Ota-090 <53591603+Ota-090@users.noreply.github.com>
2020-09-14 19:11:27 -0400

    the page of district analysis

---
+++ StarterCode0914/static/data/result2018_shon.js
@@ -0,0 +1,101 @@
+var result2018 = [
+    {
+
+       "party": "Republican Party",
+       "percent": 51.3,
+       "district": 1
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 57.6,
+       "district": 2
+
+   },
+    {
+       "party": "Democratic Party",
+       "percent": 73.6,
+       "district": 3
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 65.3,
+       "district": 4
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 62.3,
+       "district": 5
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 69.3,
+       "district": 6
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 58.7,
+       "district": 7
+
+   },
+    {
+
+       "party": "Republican Party",
+       "percent": 66.6,
+       "district": 8
+
+   },
+   {
+       "party": "Democratic Party",
+       "percent": 67.8,
+       "district": 9
+
+   },
+   {
+
+       "party": "Republican Party",
+       "percent": 55.9,
+       "district": 10
+
+   },
+    {
+       "party": "Democratic Party",
+       "percent": 82.2,
+       "district": 11
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 51.4,
+       "district": 12
+
+   },
+    {
+       "party": "Democratic Party",
+       "percent": 61.0,
+       "district": 13
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 55.2,
+       "district": 14
+
+   },
+    {
+       "party": "Republican Party",
+       "percent": 58.3,
+       "district": 15
+   },
+    {
+
+       "party": "Republican Party",
+       "percent": 56.7,
+       "district": 16
+
+   }
+]
